f631155f3069968c7d3528d23e4bae6f37a806b8
0smar <osmar.roramas@gmail.com>
2017-06-08 17:33:27 -0500

    added animations

--- js/script.js
+++ js/script.js
@@ -15,3 +15,11 @@ function openNav() {
 function closeNav() {
     $("#myNav").width("0%");
 }
+
+window.sr = ScrollReveal();
+sr.reveal('.from-bottom', { duration: 1200 });
+sr.reveal('.desc-from-bottom', { duration: 1200 }, 350 );
+sr.reveal('.recipe-from-bottom', { duration: 700 }, 250 );
+sr.reveal('.step-from-bottom', { duration: 1200 }, 350 );
+sr.reveal('.step-from-left', { origin: 'left', duration: 1800, distance: '100px' }, 550 );
+sr.reveal('.square-from-left', { origin: 'left', duration: 1800, distance: '100px' }, 350 );
